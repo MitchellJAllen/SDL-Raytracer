d77ff4a8aab2981e84fa84d4e847132d7abb69df
mitch <mitch@debianOS.debianOS>
2020-04-05 02:59:18 -0700

    makes interface a bit more readable

--- source/SDLGLHandler/SDLGLHandler.h
+++ source/SDLGLHandler/SDLGLHandler.h
@@ -4,11 +4,11 @@
 class SDLGLHandler {
 public:
 
-	virtual ~SDLGLHandler() {}; // C++ is weird
-
 	virtual void setup() = 0;
 	virtual void render() = 0;
 	virtual void clean() = 0;
+
+	virtual ~SDLGLHandler() = default;
 };
 
 #endif
